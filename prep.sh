52906988ab2c5f6f4f545b00c24850361016e68a
Juliet Mankin <jemankin@wisc.edu>
2024-04-09 11:51:05 -0500

    final functional prep.sh

--- prep.sh
+++ prep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 wget "https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespeare.tar"
-tar -xvf shakespeare.tar
+tar -xf shakespeare.tar
 cat shakespeare/*/* > all.txt
+split -n l/5 all.txt subset
